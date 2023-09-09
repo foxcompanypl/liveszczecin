1a4afe19f3ae04e601b354fd17eee5a3342682cd
Łukasz Lis <ll.lukasz.lis@gmail.com>
2023-09-09 10:48:01 +0000

    feat: release version

--- lib/src/services/camera_service.dart
+++ lib/src/services/camera_service.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import '../models/camera_model.dart';
 
@@ -12,13 +13,17 @@ class CameraService {
 
   static Future<List<CameraModel>> getAll() async {
     for (var url in urls) {
-      var response = await http.get(Uri.parse(url));
-      print("Url $url, status: ${response.statusCode}");
-      if (response.statusCode != HttpStatus.ok) {
-        continue;
+      try {
+        var response = await http.get(Uri.parse(url));
+        debugPrint("Url $url, status: ${response.statusCode}");
+        if (response.statusCode != HttpStatus.ok) {
+          continue;
+        }
+        List<dynamic> json = jsonDecode(utf8.decode(response.bodyBytes));
+        return json.map((json) => CameraModel.fromJson(json)).toList();
+      } catch (e) {
+        debugPrint(e.toString());
       }
-      List<dynamic> json = jsonDecode(utf8.decode(response.bodyBytes));
-      return json.map((json) => CameraModel.fromJson(json)).toList();
     }
     return [];
   }
